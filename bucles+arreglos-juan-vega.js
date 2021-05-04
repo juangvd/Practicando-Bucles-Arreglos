6f3b9b135c7f88c748c440fbf802950670925169
juangvd <77943731+juangvd@users.noreply.github.com>
2021-05-04 05:33:14 -0400

    Add files via upload

---
+++ bucles+arreglos-juan-vega.js
@@ -0,0 +1,74 @@
+//   Practicando Bucles + Arreglos   //
+
+//--------------- 1 ---------------
+/*
+var h = [2,4,6,8,10];
+for (var i = 0; i < h.length; i++) {
+    console.log(i);
+    console.log(h[i]);
+}
+*/
+
+
+//--------------- 2 ---------------
+/*
+var j = [2,4,6,8,10];
+for (var k = j.length - 1; k > 0; k--) {
+    console.log(k);
+    console.log(j[k]);
+}
+*/
+
+
+//--------------- 3 ---------------
+/*
+var m = [2,4,6,8,10];
+for (var n = 0; n < m.length; n += 2) {
+    console.log(n);
+    console.log(m[n]);
+}
+*/
+
+
+//--------------- 4 ---------------
+/*
+var p = [-1,0,5,-3,2];
+for (var q = 0; q < p.length; q++) {
+    if (p[q] < 0) {
+        p[q] = "Dojo";
+    }
+}
+console.log(p);
+*/
+
+
+//--------------- 5 ---------------
+/*
+var r = [-1,0,5,-3,2];
+for (var s = 0; s < r.length; s++) {
+    if (r[s] > 0) {
+        r[s] = r[s] * r[s];
+    }
+}
+console.log(r);
+*/
+
+
+//--------------- 6 ---------------
+/*
+var t = [];
+for (var u = 0; u < 4; u++) {
+    t.push(u);
+}
+console.log(t);
+*/
+
+
+//--------------- 7 ---------------
+/*
+var v = [1, 2, 3, 4, 5];
+for (var w = 0; w < v.length; w++) {
+    v.pop();
+}
+console.log(v);
+*/
